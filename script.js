abeb061eecd3bc2ac9c50e732b448a46d1ee9598
Woo7i <or2447221@naver.com>
2023-04-11 12:06:28 +0900

    json파일 정제 후 파싱하여 브라우저 요청 확인

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var req = new XMLHttpRequest();
-req.open("GET", "image_list.json"); // json 문서 파일 얻어오도록 요청
+req.open("GET", "json/image_list.json"); // json 문서 파일 얻어오도록 요청
 req.onreadystatechange = function () {
   // 콜백함수 등록 == 두번 째 인자에 부합하는 조건 값이 대입되면 함수 호출
   if (this.readyState == 4) {
